733b2633b08e494e7d9e7d4932ed6e9af170331c
Rok Mokotar <mokotar.rok@gmail.com>
2024-01-05 22:45:53 +0100

    Implement reverse geocode logic

--- controllers/reverse_geocode.py
+++ controllers/reverse_geocode.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from controllers.distance import calculate_distance
 
 
 def reverse_geocode(database: pd.DataFrame, latitude: float, longitude: float) -> str:
@@ -13,5 +14,33 @@ def reverse_geocode(database: pd.DataFrame, latitude: float, longitude: float) -
     Returns:
         str: The IP address of the geographic coordinates.
     """
-    # TODO
-    return "123.456.789.0"
+    # Find the group where the latitude and longitude belongs the nearest based on the distance
+    group = database.loc[
+        database.apply(
+            lambda row: calculate_distance(
+                latitude,
+                longitude,
+                row["ip_address_latitude"],
+                row["ip_address_longitude"],
+            ),
+            axis=1,
+        ).idxmin()
+    ]
+
+    # Get the start and end IP address of the group and calculate its midpoint
+    ip_address_start = group["ip_address_start"]
+    ip_address_end = group["ip_address_end"]
+    ip_address = (ip_address_start + ip_address_end) // 2
+
+    # Convert the IP address to string separated by dots
+    ip_address = ".".join(
+        [
+            str((ip_address >> 24) & 255),
+            str((ip_address >> 16) & 255),
+            str((ip_address >> 8) & 255),
+            str(ip_address & 255),
+        ]
+    )
+
+    # Return the IP address
+    return ip_address
